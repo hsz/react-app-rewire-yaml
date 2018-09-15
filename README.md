972aa809f02a732fc3b3f24b38e3418224994793
hsz <jakub@chrzanowski.info>
2018-09-15 10:00:07 +0200

    [1.1.0] added yaml-lint-loader, updated readme, prettier integration

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # react-app-rewire-yaml
 
+[![Dependency Status](https://img.shields.io/david/hsz/react-app-rewire-yaml.svg?style=flat-square)](https://david-dm.org/hsz/react-app-rewire-yaml)
+[![Dev Dependency Status](https://img.shields.io/david/dev/hsz/react-app-rewire-yaml.svg?style=flat-square)](https://david-dm.org/hsz/react-app-rewire-yaml)
+[![Version](https://img.shields.io/npm/v/react-app-rewire-yaml.svg?style=flat-square)](https://www.npmjs.com/package/react-app-rewire-yaml)
+[![Month Download](https://img.shields.io/npm/dm/react-app-rewire-yaml.svg?style=flat-square)](https://www.npmjs.com/package/react-app-rewire-yaml)
+[![License](https://img.shields.io/npm/l/react-app-rewire-yaml.svg?style=flat-square)](https://www.npmjs.com/package/react-app-rewire-yaml)
+
 Add [yaml-loader](https://github.com/okonet/yaml-loader) together with
+[yaml-lint-loader](https://github.com/hsz/yaml-lint-loader) and
 [json-loader](https://github.com/webpack-contrib/json-loader) to your
 [create-react-app](https://github.com/facebookincubator/create-react-app) via
 [react-app-rewired](https://github.com/timarney/react-app-rewired).
@@ -20,6 +27,7 @@ npm install --save-dev react-app-rewire-yaml
 ## Usage
 
 In your react-app-rewired configuration:
+
 ```js
 /* config-overrides.js */
 
@@ -34,7 +42,8 @@ module.exports = function override(config, env) {
 ```
 
 In your React application:
-```js
+
+```jsx
 import data from './data.yaml'
 
 const App = () => (
--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const path = require('path');
-const ruleChildren = (loader) => loader.use || loader.oneOf || Array.isArray(loader.loader) && loader.loader || [];
+
+const ruleChildren = loader =>
+  loader.use || loader.oneOf || (Array.isArray(loader.loader) && loader.loader) || [];
 
 const findIndexAndRules = (rulesSource, ruleMatcher) => {
   let result = undefined;
   const rules = Array.isArray(rulesSource) ? rulesSource : ruleChildren(rulesSource);
-  rules.some((rule, index) => result = ruleMatcher(rule) ? {
-    index,
-    rules
-  } : findIndexAndRules(ruleChildren(rule), ruleMatcher));
+  rules.some(
+    (rule, index) =>
+      (result = ruleMatcher(rule)
+        ? {
+            index,
+            rules,
+          }
+        : findIndexAndRules(ruleChildren(rule), ruleMatcher)),
+  );
   return result;
 };
 
-const createLoaderMatcher = loader => rule => rule.loader && rule.loader.indexOf(`${path.sep}${loader}${path.sep}`) !== -1;
+const createLoaderMatcher = loader => rule =>
+  rule.loader && rule.loader.indexOf(`${path.sep}${loader}${path.sep}`) !== -1;
 const fileLoaderMatcher = createLoaderMatcher('file-loader');
 
 const addBeforeRule = (rulesSource, ruleMatcher, value) => {
-  const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher)
-  rules.splice(index, 0, value)
+  const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher);
+  rules.splice(index, 0, value);
 };
 
 function rewireYAML(config) {
@@ -25,6 +33,7 @@ function rewireYAML(config) {
     use: [
       { loader: require.resolve('json-loader') },
       { loader: require.resolve('yaml-loader') },
+      { loader: require.resolve('yaml-lint-loader') },
     ],
   };
 
